2245c89eb767c87dc38d2f73ded8f894b44de1e3
Vivek Yadav <viveky259259@gmail.com>
2026-03-26 10:20:03 +0530

    Update project-launcher to 2.3.1

--- Casks/project-launcher.rb
+++ Casks/project-launcher.rb
@@ -1,6 +1,6 @@
 cask "project-launcher" do
-  version "2.2.1"
-  sha256 "PLACEHOLDER"
+  version "2.3.1"
+  sha256 "20c32bd4a1a0fa585de8a8b7ec893ffcc02e06e68b3c4ad5371bb43b365fc4bd"
 
   url "https://github.com/viveky259259/project-launcher/releases/download/v#{version}/ProjectLauncher-#{version}.dmg"
   name "Project Launcher"
